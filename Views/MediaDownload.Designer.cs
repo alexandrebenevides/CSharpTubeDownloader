1a2438df2e355a92c4c0255ed4bf8b66061d0ef1
Alexandre Benevides <alex.benevides@hotmail.com>
2024-01-03 21:41:57 -0300

    Implementando view para o download da mídia

---
+++ Views/MediaDownload.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CSharpTubeDownloader
+{
+    partial class MediaDownload
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            formTitle = new Label();
+            progressBar = new ProgressBar();
+            startButton = new Button();
+            folderBrowserDialog = new FolderBrowserDialog();
+            textBox1 = new TextBox();
+            youtubeLinkLabel = new Label();
+            downloadFormatAVI = new RadioButton();
+            downloadFormatLabel = new Label();
+            downloadFormatMP3 = new RadioButton();
+            downloadFormatMP4 = new RadioButton();
+            destinationFolderLabel = new Label();
+            textBox2 = new TextBox();
+            selectFolderButton = new Button();
+            SuspendLayout();
+            // 
+            // formTitle
+            // 
+            formTitle.AutoSize = true;
+            formTitle.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            formTitle.Location = new Point(135, 9);
+            formTitle.Name = "formTitle";
+            formTitle.Size = new Size(407, 46);
+            formTitle.TabIndex = 0;
+            formTitle.Text = "CSharpTubeDownloader";
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(12, 549);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(614, 42);
+            progressBar.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            startButton.FlatStyle = FlatStyle.Flat;
+            startButton.Location = new Point(256, 464);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(133, 51);
+            startButton.TabIndex = 2;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(18, 146);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(608, 34);
+            textBox1.TabIndex = 3;
+            // 
+            // youtubeLinkLabel
+            // 
+            youtubeLinkLabel.AutoSize = true;
+            youtubeLinkLabel.Font = new Font("Segoe UI", 12F);
+            youtubeLinkLabel.Location = new Point(12, 115);
+            youtubeLinkLabel.Name = "youtubeLinkLabel";
+            youtubeLinkLabel.Size = new Size(127, 28);
+            youtubeLinkLabel.TabIndex = 4;
+            youtubeLinkLabel.Text = "YouTube Link";
+            // 
+            // downloadFormatAVI
+            // 
+            downloadFormatAVI.AutoSize = true;
+            downloadFormatAVI.Location = new Point(18, 245);
+            downloadFormatAVI.Name = "downloadFormatAVI";
+            downloadFormatAVI.Size = new Size(55, 24);
+            downloadFormatAVI.TabIndex = 5;
+            downloadFormatAVI.TabStop = true;
+            downloadFormatAVI.Text = ".AVI";
+            downloadFormatAVI.UseVisualStyleBackColor = true;
+            // 
+            // downloadFormatLabel
+            // 
+            downloadFormatLabel.AutoSize = true;
+            downloadFormatLabel.Font = new Font("Segoe UI", 12F);
+            downloadFormatLabel.Location = new Point(12, 200);
+            downloadFormatLabel.Name = "downloadFormatLabel";
+            downloadFormatLabel.Size = new Size(170, 28);
+            downloadFormatLabel.TabIndex = 6;
+            downloadFormatLabel.Text = "Download Format";
+            // 
+            // downloadFormatMP3
+            // 
+            downloadFormatMP3.AutoSize = true;
+            downloadFormatMP3.Location = new Point(18, 275);
+            downloadFormatMP3.Name = "downloadFormatMP3";
+            downloadFormatMP3.Size = new Size(62, 24);
+            downloadFormatMP3.TabIndex = 7;
+            downloadFormatMP3.TabStop = true;
+            downloadFormatMP3.Text = ".MP3";
+            downloadFormatMP3.UseVisualStyleBackColor = true;
+            // 
+            // downloadFormatMP4
+            // 
+            downloadFormatMP4.AutoSize = true;
+            downloadFormatMP4.Location = new Point(18, 305);
+            downloadFormatMP4.Name = "downloadFormatMP4";
+            downloadFormatMP4.Size = new Size(62, 24);
+            downloadFormatMP4.TabIndex = 8;
+            downloadFormatMP4.TabStop = true;
+            downloadFormatMP4.Text = ".MP4";
+            downloadFormatMP4.UseVisualStyleBackColor = true;
+            // 
+            // destinationFolderLabel
+            // 
+            destinationFolderLabel.AutoSize = true;
+            destinationFolderLabel.Font = new Font("Segoe UI", 12F);
+            destinationFolderLabel.Location = new Point(12, 351);
+            destinationFolderLabel.Name = "destinationFolderLabel";
+            destinationFolderLabel.Size = new Size(173, 28);
+            destinationFolderLabel.TabIndex = 9;
+            destinationFolderLabel.Text = "Destination Folder";
+            // 
+            // textBox2
+            // 
+            textBox2.Enabled = false;
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(18, 382);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(478, 34);
+            textBox2.TabIndex = 10;
+            // 
+            // selectFolderButton
+            // 
+            selectFolderButton.FlatStyle = FlatStyle.Flat;
+            selectFolderButton.Location = new Point(502, 382);
+            selectFolderButton.Name = "selectFolderButton";
+            selectFolderButton.Size = new Size(124, 34);
+            selectFolderButton.TabIndex = 11;
+            selectFolderButton.Text = "Select";
+            selectFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // MediaDownload
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(638, 615);
+            Controls.Add(selectFolderButton);
+            Controls.Add(textBox2);
+            Controls.Add(destinationFolderLabel);
+            Controls.Add(downloadFormatMP4);
+            Controls.Add(downloadFormatMP3);
+            Controls.Add(downloadFormatLabel);
+            Controls.Add(downloadFormatAVI);
+            Controls.Add(youtubeLinkLabel);
+            Controls.Add(textBox1);
+            Controls.Add(startButton);
+            Controls.Add(progressBar);
+            Controls.Add(formTitle);
+            Name = "MediaDownload";
+            Text = "CSharpTubeDownloader";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label formTitle;
+        private ProgressBar progressBar;
+        private Button startButton;
+        private FolderBrowserDialog folderBrowserDialog;
+        private TextBox textBox1;
+        private Label youtubeLinkLabel;
+        private RadioButton downloadFormatAVI;
+        private Label downloadFormatLabel;
+        private RadioButton downloadFormatMP3;
+        private RadioButton downloadFormatMP4;
+        private Label destinationFolderLabel;
+        private TextBox textBox2;
+        private Button selectFolderButton;
+    }
+}
--- Views/MediaDownload.cs
+++ Views/MediaDownload.cs
@@ -1,8 +1,8 @@
 namespace CSharpTubeDownloader
 {
-    public partial class Form1 : Form
+    public partial class MediaDownload : Form
     {
-        public Form1()
+        public MediaDownload()
         {
             InitializeComponent();
         }
